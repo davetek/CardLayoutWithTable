b93bc104aa2a25397a704625f3f1ad0183ae60cf
davetek <dlawrence4994@gmail.com>
2019-10-31 18:57:54 -0400

    Initial Commit

---
+++ CardLayoutWithTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardLayoutWithTable
+//
+//  Created by David Lawrence on 10/31/19.
+//  Copyright © 2019 clarity for action. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
